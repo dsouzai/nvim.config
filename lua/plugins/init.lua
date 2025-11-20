a548740befa067bd2883b6cdfea0611e7a6742b3
Irwin D'Souza <dsouzai.gh@gmail.com>
2025-11-20 11:54:27 -0500

    todo-comments config
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,9 +2,6 @@ return {
   -- Detect tabstop and shiftwidth automatically
   -- 'NMAC427/guess-indent.nvim',
 
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
