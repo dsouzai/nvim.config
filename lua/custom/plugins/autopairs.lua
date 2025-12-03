730bbd3ebad1e9401f01de7a7f33923f83ace530
Irwin D'Souza <dsouzai.gh@gmail.com>
2025-12-02 21:04:27 -0500

    Disable autopairs for lisp
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -3,7 +3,12 @@ return {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     config = true,
+
     -- use opts = {} for passing setup options
     -- this is equivalent to setup({}) function
+    opts = {
+      -- tends to eat closing parentheses
+      disable_filetype = { 'racket', 'scheme' },
+    },
   },
 }
