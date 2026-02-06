12bfc622d56b91a17ec2286d4b26a9a06a28aa9a
Irwin D'Souza <dsouzai.gh@gmail.com>
2026-02-06 12:34:39 -0500

    Suppress warnings about global object vim
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -261,6 +261,21 @@ return {
               completion = {
                 callSnippet = 'Replace',
               },
+              diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {
+                  'vim',
+                  'require',
+                },
+              },
+              workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file('', true),
+              },
+              -- Do not send telemetry data containing a randomized but unique identifier
+              telemetry = {
+                enable = false,
+              },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
             },
