075c57dadb3b782f272ce95b52089f2992a04f7c
Irwin D'Souza <dsouzai.gh@gmail.com>
2026-02-06 12:35:25 -0500

    Add mapping to navigate gitsigns hunks
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,6 +3,17 @@ return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     enabled = true,
+    config = function()
+      if not vim.opt.diff:get() then
+        local gs = require 'gitsigns'
+        vim.keymap.set('n', ']c', function()
+          gs.nav_hunk 'next'
+        end, { desc = 'Next hunk' })
+        vim.keymap.set('n', '[c', function()
+          gs.nav_hunk 'prev'
+        end, { desc = 'Prev hunk' })
+      end
+    end,
     --[[
     opts = {
       signs = {
